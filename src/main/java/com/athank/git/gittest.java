df52af8be8d52757965c4e057a1c5dcea826be9d
hzw <hzw@qq.com>
2026-05-05 14:46:11 +0800

    push commit

--- src/main/java/com/athank/git/gittest.java
+++ src/main/java/com/athank/git/gittest.java
@@ -8,7 +8,7 @@ public class gittest {
         System.out.println("hello git3");
         System.out.println("master test");
         System.out.println("hot-fix test");
-        System.out.println("push test");
+        System.out.println("push test1");
 
 
     }
