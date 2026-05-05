4f70e1f0a8d30efd875a0918d121c4e5b522e9e8
hzw <hzw@qq.com>
2026-05-05 14:19:18 +0800

    hot-fix commit2

--- src/main/java/com/athank/git/gittest.java
+++ src/main/java/com/athank/git/gittest.java
@@ -6,6 +6,8 @@ public class gittest {
         System.out.println("hello git1");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("hot-fix test");
+
 
     }
 }
